9553795cffa9a074adbd97acca85e4544e5cab27
engahmed10 <eng.computer1@yahoo.com>
2020-10-04 14:38:34 -0700

    added cities names to dropdown thingstodos  navbar and made and adddedd syles

--- travelguide-frontend/src/thingstodo.js
+++ travelguide-frontend/src/thingstodo.js
@@ -1,10 +1,27 @@
+URL = `http://127.0.0.1:3000`
+console.log('thingstodo')
 class Thingstodo{
+    constructor(){
+
+    }
+  
+  static drowpdownCiteis=()=>
+  {
+   //  const  nameOfCities = document.querySelector(".names-of-cities")
+    new ApiAjax(URL,'cities').fetchAllCities().then(data=>{
+      data.forEach(el => {
+            const div = document.createElement('div')
+            div.classList.add("city")
+            let a = `<a class="city-name" > ${el.name}</a>`
+            div.setAttribute('data-id', `${el.id}`)
+            div.innerHTML += a
+            const nameOfCities = document.querySelector(".names-of-cities")
+            nameOfCities.appendChild(div)
 
 
-
-
-
-
-
+        })
+    })
+  }
     
 }
+Thingstodo.drowpdownCiteis()
